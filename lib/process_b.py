2ef0befcb5fb189746a0bb355e8b6a4884868f18
kristian <mlk299@pm.me>
2024-03-05 20:30:20 +0100

    logistic reg. model task 2

--- lib/process_b.py
+++ lib/process_b.py
@@ -1,4 +1,5 @@
 import lib.process_methods as pm
+import swifter
 
 
 def preprocess(dataframe):
@@ -19,3 +20,29 @@ def preprocess(dataframe):
     dataframe['content'] = dataframe['content'].apply(pm.remove_word_variations)
 
     return None
+
+
+
+
+
+
+
+
+# def preprocess_prll(dataframe):
+#     """
+#     Preprocessing pipeline for cleaning the data
+#     """
+
+#     # remove empty content
+#     dataframe.dropna(subset=['content'], inplace=True)
+
+#     # cleanup text on 'content' column and add into new column 'content_clean'
+#     dataframe['content'] = dataframe['content'].swifter.apply(pm.clean_text)
+
+#     # Apply remove_stopwords to 'content_clean' column and create 'content_stopword' column
+#     dataframe['content'] = dataframe['content'].swifter.apply(pm.remove_stopwords)
+
+#     # stemming
+#     dataframe['content'] = dataframe['content'].swifter.apply(pm.remove_word_variations)
+
+#     return None
